bab86c793838103f948c49aa0d515fcb9d267ada
Edin Jašarević <edinjasar14@gmail.com>
2017-08-03 00:05:12 +0200

    primjer36-01

---
+++ ipcs_36/primjer01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace primjer01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarisemo kolekciju tipa ArrayList
+            ArrayList names = new ArrayList();
+            Console.WriteLine("Incjalizovani broj elemenata: " + names.Count + "\n");
+
+            //dodoavanje elemenata u listu
+            names.Add("Andjela");
+            names.Add("Nevena");
+            names.Add("Ana");
+
+            Console.WriteLine("Broj elemenata nakon dodavanje {0}\n", names.Count);
+
+            //prikazivanje cijele liste koriscenjem indeksera
+            Console.WriteLine("Trenutni sadrzaj: ");
+            for (int i = 0; i < names.Count; i++)
+                Console.Write(names[i] + " ");
+        }
+    }
+}
