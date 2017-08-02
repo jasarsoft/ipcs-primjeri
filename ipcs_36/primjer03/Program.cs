224988ce3fa81128721c1b4710e025a67cc8a5a1
Edin Jašarević <edinjasar14@gmail.com>
2017-08-03 00:36:34 +0200

    primjer36-03

---
+++ ipcs_36/primjer03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace primjer03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciranje objekta klase Stack
+            Stack stack = new Stack();
+
+            //dodavanje elemenata na stek
+            stack.Push(11);
+            stack.Push(22);
+            stack.Push(33);
+
+            //prikazivanje elemenata steka
+            foreach (int i in stack) Console.Write(i + " ");
+            Console.WriteLine();
+
+            //brisanje elementa sa steka
+            stack.Pop();
+            foreach (int i in stack) Console.Write(i + " ");
+            Console.Read();
+        }
+    }
+}
