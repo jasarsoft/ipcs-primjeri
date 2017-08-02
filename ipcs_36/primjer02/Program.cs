b7b66ba10d233346dfdc34388f0d8998930ca8b7
Edin Jašarević <edinjasar14@gmail.com>
2017-08-03 00:29:36 +0200

    primjer36-02

---
+++ ipcs_36/primjer02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace primjer02
+{
+    class QueneDemo
+    {
+        static void Main(string[] args)
+        {
+            //dodajemo instancu klase Quene
+            Queue quene = new Queue();
+
+            //dodajemo vrijednosti u red
+            quene.Enqueue(33);
+            quene.Enqueue(55);
+            quene.Enqueue(66);
+
+            //prikazujemo sve vrijednosti iz reda
+            foreach (int i in quene) Console.Write(i + " ");
+            Console.WriteLine();
+
+            //brisemo prvu vrijednost iz reda
+            quene.Dequeue();
+
+            //prikazujemo sve vrijednosti iz reda
+            foreach (int i in quene) Console.Write(i + " ");
+
+            Console.Read();
+        }
+    }
+}
