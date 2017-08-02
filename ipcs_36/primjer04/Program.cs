7c21d8baf72934d960a215e47b41888560a7e5f1
Edin Jašarević <edinjasar14@gmail.com>
2017-08-03 00:45:21 +0200

    primjer36-04

---
+++ ipcs_36/primjer04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace primjer04
+{
+    class HashtableDemo
+    {
+        static void Main(string[] args)
+        {
+            //kreiramo instancu klase Hashtable
+            Hashtable hash = new Hashtable();
+
+            //dodajemo elemente u hashtabelu
+            hash.Add("prvo", "Beograd");
+            hash.Add("drugo", "Novi Sad");
+            hash.Add("trece", "Rume");
+
+            //mozemo koristiti i indeksere za dodavanje elemenata
+            hash["cetrvrto"] = "Sremska Kamenica";
+
+            //uzima kljuceve iz kolekcije
+            ICollection c = hash.Keys;
+
+            //na osnvu predhodni uzetih kljuceva dobijamo vrijednost
+            foreach (string str in c) Console.WriteLine(str + ": " + hash[str]);
+
+            Console.Read();
+        }
+    }
+}
