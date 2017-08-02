fb39be33c28e0cce3bae68af4798b27bef6eaf3d
Edin Jašarević <edinjasar14@gmail.com>
2017-08-03 01:29:33 +0200

    vjezba36-01

---
+++ ipcs_36/vjezba01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//obavezno navodimo prostor imena System.Collections kako bi smo 
+//mogli da koristimo klase kolekcija
+using System.Collections;
+
+namespace vjezba01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarisemo deklaraciju tipa ArrayList
+            ArrayList cities = new ArrayList();
+
+            //dodajemo svaki grad posebno
+            cities.Add("Novi Sad");
+            cities.Add("Beograd");
+            cities.Add("Nis");
+
+            //brisemo grad nis
+            cities.Remove("Nis");
+
+            //iscitava sva imena
+            foreach (string city in cities)
+                Console.WriteLine(city);
+
+            Console.Read();
+        }
+    }
+}
