20f99c24b9fe0284f97560705727564b956d672d
Edin Jašarević <edinjasar14@gmail.com>
2017-08-04 01:20:22 +0200

    primjer38-01

---
+++ ipcs_38/primjer01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+//na ovaj nacin deklarisemo korisnicki interfejs
+interface IExample<T>
+{
+    //deklarisemo neku metodu koju taj interfejs moze da implementira
+    T getValue(T val);
+}
+
+//deklarisemo genericku klasu koja implementira nas genericki interfejs
+class NewClass<T> : IExample<T>
+{
+    public T getValue(T val)
+    {
+        return val;
+    }
+}
+
+namespace primjer01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciramo objekat klase MyClass sa parametrom tipa string
+            NewClass<string> mytext = new NewClass<string>();
+
+            //ovaj kod je dodat samo za potrebe objasnjavanja invarijansa
+            NewClass<string> text = mytext;
+
+            Console.WriteLine("{0}", mytext.getValue("Dobar Dan!"));
+            Console.Read();
+        }
+    }
+}
