6a0eb5b60ffce31b54339403922cb684bbd18c8b
Edin Jašarević <edinjasar14@gmail.com>
2017-08-04 23:08:12 +0200

    primjer39-01

---
+++ ipcs_39/primjer01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace primjer01
+{
+    class Program
+    {
+        public static void MyMethod<T>(T val)
+        {
+            Console.WriteLine(val);
+        }
+
+        static void Main(string[] args)
+        {
+            //vidimo kako se ponasa metoda kada joj proslijedimo sve ove tipove
+            int intVal = 1;
+            double doubleVal = 3.14;
+            bool boolVal = true;
+            string stringVal = "Hello!";
+
+            MyMethod(intVal);
+            MyMethod(doubleVal);
+            MyMethod(boolVal);
+            MyMethod(stringVal);
+
+            Console.Read();
+        }
+    }
+}
