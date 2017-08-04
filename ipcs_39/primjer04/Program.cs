d32697aa32bea3294c0903fed2e1c965911caf1c
Edin Jašarević <edinjasar14@gmail.com>
2017-08-04 23:45:19 +0200

    primjer39-04

---
+++ ipcs_39/primjer04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace primjer04
+{
+    class GenericFunc
+    {
+        static void Main(string[] args)
+        {
+            //instanciranje delegata da referise na nasu metodu
+            //za povratni tip definisemo da to bude string
+            Func<string, string> MyMethod = LowercaseString;
+
+            string city = "SARAJEVO";
+            Console.WriteLine(MyMethod(city));
+
+            Console.Read();
+        }
+
+        //nasa metoda koja vrsi konverziju iz velikih u mala slova
+        private static string LowercaseString(string str)
+        {
+            return str.ToLower();
+        }
+    }
+}
