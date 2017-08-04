b14007a3c920ebe4abb286c9e530dc8ad80a6e3f
Edin Jašarević <edinjasar14@gmail.com>
2017-08-04 22:51:50 +0200

    vjezba38-01

---
+++ ipcs_38/vjezb01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+//pravimo genericki interfejs koji podrzava kovarijnsu
+public interface ICov<out T>
+{
+    T GetObject();
+}
+
+//MyClass implementira interfejs ICov
+class MyClass<T> : ICov<T>
+{
+    T obj;
+    
+    public MyClass(T v)
+    {
+        obj = v;
+    }
+
+    public T GetObject()
+    {
+        return obj;
+    }
+}
+
+//kreiramo jednostavno hijerarhiju klasa
+class People
+{
+    string name;
+
+    public People(string n)
+    {
+        name = n;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+} 
+
+class Student : People
+{
+    public Student(string n) : base(n)
+    {
+    }
+}
+
+namespace vjezb01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kreiramo MyClass  objekat tipa Student i dodjelimo ga instanci
+            //interfejsa ICov koji je nadtipa People
+            ICov<People> Ref = new MyClass<Student>(new Student("Marko Markovic"));
+            Console.WriteLine("Novi student je: " + Ref.GetObject().GetName());
+
+            Console.Read();
+        }
+    }
+}
