953b61f2a3fbbf46c257b4224a7a6a5e6a2c4d7e
Edin Jašarević <edinjasar14@gmail.com>
2017-08-04 23:58:24 +0200

    vjezba39-02

---
+++ ipcs_39/vjezba02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace vjezba02
+{
+    public class Program
+    {
+        public static void MyMethod<U, V>(U val1, V val2)
+        {
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+        }
+
+        static void Main(string[] args)
+        {
+            int intVal1 = 1;
+            int intVal2 = 3;
+
+            MyMethod(intVal1, intVal2);
+
+            Console.Read();
+        }
+    }
+}
