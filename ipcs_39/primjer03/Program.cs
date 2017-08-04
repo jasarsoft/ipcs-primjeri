cfb764932374c209781dd84a0ad33a90900c2fe9
Edin Jašarević <edinjasar14@gmail.com>
2017-08-04 23:44:01 +0200

    primjer39-03

---
+++ ipcs_39/primjer03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace primjer03
+{
+    class Program
+    {
+        static void Main()
+        {
+            //uzimamo instancu delegata Action, tipa string
+            Action<String> example = new Action<String>(Program.Show);
+
+            //instanca objekta kolekcija List-a
+            List<String> cities = new List<String>
+            {
+                "Novi Sad", "Beograd", "Nis"
+            };
+
+            cities.ForEach(example);
+
+            Console.Read();
+        }
+
+        static void Show(String s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
