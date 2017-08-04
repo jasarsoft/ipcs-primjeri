27b4c27ce7ad35ba6b19db8bb0d5a6c6243ee23d
Edin Jašarević <edinjasar14@gmail.com>
2017-08-04 22:14:37 +0200

    primjer38-03

---
+++ ipcs_38/primjer03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+//prvo genericki interfejs koji podrzava kovarijansu
+public interface ICov<in T>
+{
+    void Show(T obj);
+}
+
+//MyClass implementira interfejs ICov
+class MyClass<T> : ICov<T>
+{
+    public void Show(T x)
+    {
+        Console.WriteLine(x);
+    }
+}
+
+//kreiramo jednostavnu hijerarhiju klasa
+class Animal
+{
+    public override string ToString()
+    {
+        return "Ovo je zivotivnja.";
+    }
+}
+class Horse : Animal
+{
+    public override string ToString()
+    {
+        return "Ovo je zivotirnja";
+    }
+}
+
+namespace primjer03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kreirmao MyClass objekat tipa Animal dodjelimo ga instanci interfejsa
+            //ICov tipa Animal
+            ICov<Animal> Ref = new MyClass<Animal>();
+
+            //kreiramo objekat MyClass<Animal> i dodjelimo ga Ref2 promjenljivoj koja je
+            //instanca interfejsa ICov<Horse>
+            ICov<Horse> Ref2 = new MyClass<Animal>();
+            Ref2.Show(new Horse());
+
+            Console.Read();
+        }
+    }
+}
