92b9c657f1c8eb40c7df37255de9aedc0884d0d9
Edin Jašarević <edinjasar14@gmail.com>
2017-08-05 11:19:24 +0200

    primjer40-02

---
+++ ipcs_40/primjer02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace primjer02
+{
+    public class Student
+    {
+        public string Name;
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, Student> students = new Dictionary<string, Student>();
+
+            students.Add("Student1", new Student("Petar Petrovic"));
+            students.Add("Student2", new Student("Nemanja Nemanjic"));
+            students.Add("Student3", new Student("Mirko Mirkovic"));
+
+            foreach (Student student in students.Values)
+            {
+                Console.WriteLine(student.Name);
+            }
+
+            Console.Read();
+        }
+    }
+}
