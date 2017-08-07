2ef4ef4db0f886a1b7c4fbaacf20ff94e80a3ea4
Edin Jašarević <edinjasar14@gmail.com>
2017-08-07 17:30:21 +0200

    primjer40-05

---
+++ ipcs_40/primjer05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace primjer05
+{
+    class MyClass
+    {
+        string[] cities = { "BGD", "NS" };
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (string city in cities)
+                yield return city;
+        }
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            MyClass example = new MyClass();
+
+            foreach (string city in example)
+                Console.Write(city + ", ");
+
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
