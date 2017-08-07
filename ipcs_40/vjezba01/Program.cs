0a127b6f0ff40b7886165e1fccf073bcef838054
Edin Jašarević <edinjasar14@gmail.com>
2017-08-07 17:42:51 +0200

    vjezba40-01

---
+++ ipcs_40/vjezba01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace vjezba01
+{
+    class EnumClass
+    {
+        char[] letters = { 'A', 'B', 'V', 'G', 'D', 'Đ' };
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (char letter in letters)
+                yield return letter;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            EnumClass let = new EnumClass();
+
+            foreach (char letter in let)
+                Console.Write(letter + " ");
+
+            Console.Read();
+        }
+    }
+}
