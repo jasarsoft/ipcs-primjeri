71d2056c3b8e0dd3656a23bdc265f7f0049ead97
Edin Jašarević <edinjasar14@gmail.com>
2017-08-07 18:06:56 +0200

    primjer41-02

---
+++ ipcs_41/primjer01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+
+namespace primjer01
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] Age = { 11, 10, 9, 6, 18, 23, 48 };
+
+            //upit koji propusta (odnosno koji vraca) samo one osobe koje imaju 18 i vise godina
+            var ages = from a in Age where a >= 18 select a;
+
+            Console.Write("Samo osobe sa ovim godina mogu kupovati: ");
+
+            //izvrsavanje upita ages
+            foreach (int i in ages)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
