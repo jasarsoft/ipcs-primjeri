985ba478b15e41236f9e9aa2a42bfcbf31fd8a72
Edin Jašarević <edinjasar14@gmail.com>
2017-08-07 18:12:11 +0200

    primjer40-02

---
+++ ipcs_41/primjer02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+
+namespace primjer02
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] sort = { 2, 8, 4, 6, 7, 5, 3, 1, 9, 10 };
+
+            Console.Write("Niz: ");
+            foreach (int i in sort)
+                Console.Write(i + " ");
+            Console.WriteLine();
+            //upit koji sortira brojeve u rastucem poretku iz niza sort
+            var sorts = from s in sort orderby s select s;
+
+            Console.Write("Brojevi od 1-10: ");
+
+            foreach (int i in sorts)
+                Console.Write(i + " ");
+
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
