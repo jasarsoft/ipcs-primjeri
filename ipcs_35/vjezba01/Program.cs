1cf445290c91bf485cf9ef7c2445d3f785f87414
Edin Jašarević <edinjasar14@gmail.com>
2017-08-08 21:02:44 +0200

    vjezba35-01

---
+++ ipcs_35/vjezba01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Vjezba1
+{
+    //definisemo delegat
+    delegate void MyDelegate();
+
+    class MyEvent : Form
+    {
+        //definisemo dogadjaj
+        event MyDelegate EventExample;
+
+        public MyEvent()
+        {
+            Button b1 = new Button();
+            b1.Parent = this;
+            b1.Text = "Desio se";
+
+            //dodjeljujemo nas delegat dogadjaju EventExample
+            //tj. registrujemo ga na dogadjaj EventExample
+            EventExample += new MyDelegate(OnStartEvent);
+
+            //zapocinje nas dogadjaj
+            EventExample();
+        }
+
+        //ovja metod se poziva kad se dogodi "EventExample" : Event handelr
+        public void OnStartEvent()
+        {
+            MessageBox.Show("Hej! Klikni na ok da bi vidjeo kako funkcionise dogadjaj!");
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyEvent());
+        }
+    }
+}
