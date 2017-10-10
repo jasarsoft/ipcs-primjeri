24430029d2b2dc8c3a1feaa13182efe38b3e8e9e
Edin Jašarević <edinjasar14@gmail.com>
2017-10-11 00:01:16 +0200

    ipcs33 primjer2

---
+++ ipcs_33/primjer02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primjer2
+{
+    delegate string Example();
+
+    public class Practice
+    {
+        public string Insturctor()
+        {
+            return "Zdravo! Zovem se Petra Petrovic i ja sam predavac!";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kreiramo instancu klase practicle
+            Practice obj = new Practice();
+
+            //kreiramo instancu delegata
+            Example examp = obj.Insturctor;
+
+            //pozivanje metode
+            string str = examp();
+
+            Console.WriteLine("Ovo je primjer: " + str);
+            Console.ReadKey();
+
+        }
+    }
+}
