ab03de540db5f86de33e82d0d37c9f8db4829f71
Edin Jašarević <edinjasar14@gmail.com>
2017-10-11 00:00:21 +0200

    ipcs33 vjezba1

---
+++ ipcs_33/vjezba01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba1
+{
+    public delegate int Putarina (int tipVozila);
+
+    public class Metods
+    {
+        public int Naplata (int tipVozila)
+        {
+            if (tipVozila == 1)
+                return 200;
+            else
+                return 250;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metods met = new Metods();
+
+            //instanciranje
+            Putarina put = met.Naplata;
+
+            //pozivanje metode
+            //postavili smo vrijednost voizila na tip 1
+            Console.WriteLine("Putarina iznosi: {0}", put(1));
+            Console.ReadKey();
+        }
+    }
+}
