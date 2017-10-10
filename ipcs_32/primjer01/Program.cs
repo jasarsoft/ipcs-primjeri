3f7bccc3c2a759a00e71b7c1e960340635ac8cab
Edin Jašarević <edinjasar14@gmail.com>
2017-10-11 00:02:21 +0200

    ipcs32 primjer1

---
+++ ipcs_32/primjer01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_32_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(2, 22);
+            Vector v2 = new Vector(3, 2);
+            Vector v3 = new Vector();
+
+            v3 = v1 + v2;
+
+            Console.WriteLine("Vrijednost sabiranja vektora: {0}, {1}", v3.X, v3.Y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ipcs_32/primjer01/Vector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_32_P1
+{
+    public class Vector
+    {
+        public int X;
+        public int Y;
+
+
+        //konstruktor bez parametara
+        public Vector() {}
+
+        //konstruktor sa prametrima
+        public Vector(int i, int j)
+        {
+            X = i;
+            Y = j;
+        }
+
+        //prekoracenje za binrni operator +
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+    }
+}
