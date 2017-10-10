b2cb9b636b5926be2de09a69174116257485ca7c
Edin Jašarević <edinjasar14@gmail.com>
2017-10-11 00:01:42 +0200

    ipcs32 vjezba1

---
+++ ipcs_32/vjezba01/Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_1
+{
+    class Number
+    {
+        private int value;
+
+        public Number(int val)
+        {
+            value = val;
+        }
+
+        public static Number operator+(Number arg1, Number arg2)
+        {
+            return new Number(arg1.value + arg2.value);
+        }
+
+        //deklarisemo prekoracenje operatora *
+        //nad jednim operandom tima Number i drugim tima int
+        public static Number operator* (Number one, int two)
+        {
+            return new Number(one.value * two);
+        }
+    }
+}
---
+++ ipcs_32/vjezba01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
