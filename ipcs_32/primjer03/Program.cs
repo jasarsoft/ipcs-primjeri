a0966d9106975598f161785323147c5f279940b3
Edin Jašarević <edinjasar14@gmail.com>
2017-10-11 00:01:55 +0200

    ipcs32 primjer3

---
+++ ipcs_32/primjer03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primer_3
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Values m = new Values(3, 2, 1);
+            Values n = new Values(1, 2, 3);
+            Values v = new Values();
+
+            //int i;
+
+            //i = m;
+            //v = m + n;
+
+            int i;
+
+            i = (int)m; //vrsimo kastovanje
+            Console.WriteLine("Result of i = a: " + i);
+
+        }
+    }
+}
---
+++ ipcs_32/primjer03/Values.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primer_3
+{
+    //deklarisimo klasu u kojoj ce konverzioni operator biti definisan
+    class Values
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        //definisimo konstrukotr za klasu Values koji ne uzima argumente
+        public Values() { }
+
+        //definisimo konstruktor za klasu Values koji uzima arguemnte
+        public Values(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        //definisimo implicitnu konverziju iz Values u int
+        //public static implicit operator int(Values op1)
+        //{
+        //    return op1.x * op1.y * op1.z;
+        //}
+
+        //definisimo eksplicitnu konverziju iz Values u int
+        public static explicit operator int(Values op1)
+        {
+            return op1.x * op1.y * op1.z;
+        }
+    
+    }
+}
