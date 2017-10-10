5df3a882eb3948520bffb39fe6beccf8b07d3f75
Edin Jašarević <edinjasar14@gmail.com>
2017-10-11 00:00:34 +0200

    ipcs33 primjer5

---
+++ ipcs_33/primjer05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primjer5
+{
+    //primjetimo da nas delegat ima parametar
+    //posto ga i anonimna metoda ima
+    delegate void Numbers(int end);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prosliedjujemo argument anonimnoj metodi
+            Numbers numb = delegate(int end)
+            {
+                for (int i = 10; i >= end; i--) Console.WriteLine(i);
+            };
+
+            Console.WriteLine("I odbrojavanje pocinje sada!");
+            numb(1);
+            
+            Console.ReadKey();
+        }
+    }
+}
