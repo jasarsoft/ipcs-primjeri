54802344610d1bdaa8a1e63ec3f61d4764101b59
Edin Jašarević <edinjasar14@gmail.com>
2017-10-10 23:59:12 +0200

    ipcs34 vjezba1

---
+++ ipcs_34/vjezba01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba1
+{
+    //deklarisemo delegat koji ima dva parametra
+    //oba tima int i ciji je povratni tip takodje int
+    delegate int Subtraction (int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pravimo instancu delegatu Operator i dodjeljujemo joj labda izraz
+            //koji vrsi oduzimanje dva parametra koja joj se prosljedjuju
+            //i vraca rezultat koji je tipa int
+            Subtraction Operation = (x, y) =>
+            {
+                return x - y;
+            };
+
+            //pozivamo lambda izraz preko instance delegata i ispisujemo rezultat
+            Console.WriteLine("Rezultat za 4 i 3 je: {0}", Operation(4, 3));
+            Console.Read();
+        }
+    }
+}
