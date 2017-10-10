33a57ed0ff42f60f51b69123f57358a78fb91aa0
Edin Jašarević <edinjasar14@gmail.com>
2017-10-10 23:59:42 +0200

    ipcs34 primjer2

---
+++ ipcs_34/primjer02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primjer2
+{
+    //deklarisemo delagat
+    delegate bool Parity(int v);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parity n = test => test % 2 == 0;
+
+            //sada cemo upotrijebiti lambda izraz
+            //preko instance delegata test
+
+            if (n(6))
+                Console.WriteLine("Ovo je paran broj");
+            else
+                Console.WriteLine("Nije paran broj.");
+
+            Console.Read();
+        }
+    }
+}
