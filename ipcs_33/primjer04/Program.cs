7809689d53bfde3480e6ff100b1edda28183e89e
Edin Jašarević <edinjasar14@gmail.com>
2017-10-11 00:00:54 +0200

    ipcs33 primjer4

---
+++ ipcs_33/primjer04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primjer4
+{
+    //deklarisemo delegat, negov tip i argumente (ovdje ih nema)
+    public delegate void Example();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dodajemo anonimnu metodu delegatu
+            Example message = delegate
+            {
+                //stavimo da anonimna metoda nesto radi
+                //u ovom slucaju ispisuje ovaj tekst izmedju navodinika
+                Console.WriteLine("Zdravo! Zovem se Petra Petrovic i ja sam predavac!");
+            };
+
+            //pozivamo metodu
+            message();
+        
+        }
+    }
+}
