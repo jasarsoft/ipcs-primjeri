e6469561b68092432e53aaee2f0bf4a18c522c0b
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:34:05 +0200

    ipcs12 vjezba01

---
+++ ipcs_12/vjezba01/IPCS_12-V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_12_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ulazni podaci
+            int vehicleCode = 1;
+            int speed = 30;
+            String[] vehicle = { "truck", "bus", "car" };
+
+            if (vehicle[vehicleCode] != "car")
+            {
+                switch (speed)
+                {
+                    //RJESENJE 1
+                    //case 10:
+                    //    Console.WriteLine(vehicle[vehicleCode] + " speed is 10");
+                    //    break;
+                    //case 20:
+                    //    Console.WriteLine(vehicle[vehicleCode] + " speed is 20");
+                    //    break;
+                    //case 30:
+                    //    Console.WriteLine(vehicle[vehicleCode] + " speed is 30");
+                    //    break;
+                    //default:
+                    //    Console.WriteLine(vehicle[vehicleCode] + " speed is not 10, 20 or 30");
+                    //    break;
+
+                    //RJESENJE 2
+                    case 10:
+                    case 20:
+                    case 30:
+                        Console.WriteLine(vehicle[vehicleCode] + " speed is " + speed);
+                        break;
+                    default:
+                        Console.WriteLine(vehicle[vehicleCode] + " is not 10, 20 or 30");
+                        break;
+                }
+            }
+        }
+    }
+}
