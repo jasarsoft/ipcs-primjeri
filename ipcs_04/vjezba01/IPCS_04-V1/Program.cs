abae52cf9713f3d3dcb1a1a04aa40247d07171e8
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:30:29 +0200

    ipcs04 vjezba01

---
+++ ipcs_04/vjezba01/IPCS_04-V1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schools
+{
+    class Info
+    {
+        public static void Show()
+        {
+            System.Console.WriteLine("Info about schools");
+        }
+    }
+}
+
+namespace Universities
+{
+    class Info
+    {
+        public static void Show()
+        {
+            System.Console.WriteLine("Info about universities");
+        }
+    }
+}
+
+namespace IPCS_04_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schools.Info.Show();
+            Universities.Info.Show();
+        }
+    }
+}
