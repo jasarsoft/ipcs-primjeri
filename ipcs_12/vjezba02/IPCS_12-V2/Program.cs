d07f430c23ce39e6a0acc1f074e6cfdf04170b51
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:34:20 +0200

    ipcs12 vjezba02

---
+++ ipcs_12/vjezba02/IPCS_12-V2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IPCS_12_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ulazni podaci
+            int x = 523132;
+            int y = 325423;
+            int reminder = x % y;
+
+            switch (reminder)
+            {
+                case 0:
+                    Console.WriteLine("no reminder");
+                    break;
+                default:
+                    if (reminder > 1000)
+                        Console.WriteLine("reminder above 1000");
+                    else
+                        Console.WriteLine("reminder below 1000");
+                    break;
+
+            }
+        }
+    }
+}
