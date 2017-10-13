f067419a18fe257f62f5b3b57c41c048e4c43993
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:34:37 +0200

    ipcs12 vjezba03

---
+++ ipcs_12/vjezba03/IPCS_12-V3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IPCS_12_V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = { 2, 5, 3 };
+            int avg = (grades[0] + grades[1] + grades[2]) / grades.Length;
+
+            if (avg > 1)
+                Console.WriteLine("Grade " + avg + " is passing grade");
+            else
+                Console.WriteLine("Grade " + avg + " is not passing grade");
+        }
+    }
+}
