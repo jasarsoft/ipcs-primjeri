4f9e084439ca7dcf4640861874845ed2bd95b17c
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:30:13 +0200

    ipcs08 vjezba01

---
+++ ipcs_08/vjezba01/IPCS_08-V1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_08_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //amortizacija i cekanje korisnicog unosa
+            string line;
+            //iscitavanje reda teksta sa tastature
+            while ((line = Console.ReadLine()) != null)
+            {
+                //formatiranje podatak
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+
+                //ispisivanje rezultata u konzolnom prozoru
+                Console.WriteLine(line);    
+            }
+
+            
+
+        }
+    }
+}
