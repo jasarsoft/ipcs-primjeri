4efb2a3c9d9bdf00f9d2494aee27f99b7fe78620
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:31:01 +0200

    ipcs10 vjezba02

---
+++ ipcs_10/vjezba01/IPCS_10-V1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_10_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //primjer 1
+            //sabiranje dva broja
+            //dodjeljivanje vrijednosti promjenljivoj x
+            int x = 1;
+            //dodjeljivanje vrijednosti promjenjljivoj y
+            int y = 2;
+            //dodjeljivanje vrijednosti promjenjljivoj z kroz sabiranje x i y
+            int z = x + y;
+            //emitovanje rezultata
+            Console.WriteLine(z);
+        }
+    }
+}
