2aa1a9ea7a57abd0bfa6e93f6a2368b944c977cb
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:34:55 +0200

    ipcs12 vjezba04

---
+++ ipcs_12/vjezba04/IPCS_12-V4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IPCS_12_V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] status = { "stopped", "moving", "lost", "turned off", "sos" };
+            string currentStatus = status[0];
+            int speed = 25;
+
+            if (currentStatus == "moving")
+            {
+                if (speed > 20)
+                    Console.WriteLine("Speed limit broken");
+                else
+                    Console.WriteLine("Speed limit ok");
+            }
+            else
+                Console.WriteLine(currentStatus);
+        }
+    }
+}
