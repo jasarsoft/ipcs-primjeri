b8ceb38bf09de2b7ecf4cd78db8793af00f3a98b
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:30:46 +0200

    ipcs04 vjezba02

---
+++ ipcs_04/vjezba02/IPCS_04-V2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Animals
+{
+    namespace Cats
+    {
+        namespace Domestic
+        {
+            class Info { public static void Show() { Console.WriteLine("Miaou"); } }
+        }
+
+        namespace Wild
+        {
+            class Info { public static void Show() { Console.WriteLine("Grrr"); } }
+        }
+    }
+
+    namespace Dogs
+    {
+        namespace Domestic
+        {
+            class Info { public static void Show() { Console.WriteLine("Wof"); } }
+        }
+
+        namespace Wild
+        {
+            class Info { public static void Show() { Console.WriteLine("Grr"); } }
+        }
+    }
+}
+
+namespace IPCS_04_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals.Cats.Domestic.Info.Show();
+            Animals.Cats.Wild.Info.Show();
+            Animals.Dogs.Domestic.Info.Show();
+            Animals.Dogs.Wild.Info.Show();
+        }
+    }
+}
