1259070a340cc97fa3d83142e7e5841c796e5e8e
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:31:37 +0200

    ipcs10 vjezba03

---
+++ ipcs_10/vjezba03/IPCS_10-V3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_10_V3
+{
+    class UserData
+    {
+        static void Main(string[] args)
+        {
+            //VERZIJA 1
+            String firstName = "Peter";
+            String lastName = "Jackson";
+            String birthDate = "01/01/1980";
+            //String data = "First name: " + firstName + ", Last name: " + lastName + ", Birth data: " + birthDate;
+            //Console.WriteLine(data);
+
+            //VERZIJA 2
+            StringBuilder data = new StringBuilder();
+            data.Append("First name: " + firstName);
+            data.Append(", Last name: " + lastName);
+            data.Append(", Birth date: " + birthDate);
+            Console.WriteLine(data);
+        }
+    }
+}
