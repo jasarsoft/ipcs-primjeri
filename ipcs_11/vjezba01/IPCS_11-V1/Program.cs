1975fc14bc08e086581e5dfae7aefa09969bf906
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:31:55 +0200

    ipcs11 vjezba01

---
+++ ipcs_11/vjezba01/IPCS_11-V1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_11_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = new int[,] 
+            {
+                {1, 2, 3},
+                {5, 6, 7},
+                {9, 0, 1}
+            };
+
+            int[,] matrix2 = new int[,]
+            {
+                {4, 1, 6},
+                {2, 3, 5},
+                {7, 4, 3}
+            };
+
+            int[,] matrix3 = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix3[i, j] = matrix1[i, j] + matrix2[i, j];
+                    Console.Write("{0,3}", matrix3[i, j]);
+                }
+                Console.WriteLine();
+            }
+                
+
+                    
+
+            
+        }
+    }
+}
