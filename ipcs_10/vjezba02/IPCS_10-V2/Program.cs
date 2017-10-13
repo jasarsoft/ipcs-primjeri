e9f299d4104fe27e05f919845f01665f26e4001a
Edin Jašarević <edinjasar14@gmail.com>
2017-10-14 01:31:15 +0200

    ipcs10 vjezba03

---
+++ ipcs_10/vjezba02/IPCS_10-V2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_10_V2
+{
+    class Program
+    {
+        const double PI = 3.14;
+
+        static void Main(string[] args)
+        {
+            //definicija promjenljive r
+            double r = 5;
+            //definicija primjenjljive p kao rezultat izracunavanja povrsine kruga
+            double p = (r * r) * PI;
+            //emitovanje primjenljive p koja sadrzi rezultat povrsinu kruga sa poluprecinkom r
+            Console.WriteLine(p);
+        }
+    }
+}
