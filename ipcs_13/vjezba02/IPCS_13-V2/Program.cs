f0bbbc51e8cc208092f01a6fd8dcad49a4dbec8f
Edin Jašarević <edinjasar14@gmail.com>
2017-10-16 18:57:03 +0200

    ipcs13 vjezba03

---
+++ ipcs_13/vjezba02/IPCS_13-V2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IPCS_13_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, -1, 4, 5, -1 };
+            int[] arr1 = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == -1)
+                    arr1[i] = 0;
+                else
+                    arr1[i] = arr[i];
+            }
+
+            foreach (int n in arr1)
+                Console.WriteLine(n);
+        }
+    }
+}
