952b3dd789bbc3e287176f7683e112de2501e1db
Edin Jašarević <edinjasar14@gmail.com>
2017-10-16 18:57:26 +0200

    ipcs13 vjezba02

---
+++ ipcs_13/vjezba03/IPCS_13-V3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IPCS_13_V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] users = 
+            {
+                new string[] { "Peter", "Smith", "1111"},
+                new string[] {"Sally", "Simmons", "2222"},
+                new string[] {"Ross", "Richards", "3333"}
+            };
+
+            string[] names = new string[users.Length];
+
+            for (int i = 0; i < users.Length; i++)
+                names[i] = users[i][0];
+
+            foreach (string name in names)
+                Console.WriteLine(name);
+        }
+    }
+}
