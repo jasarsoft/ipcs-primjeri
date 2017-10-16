ab04ae21b1a1a2cb389cffe972c2b86bb2949e3a
Edin Jašarević <edinjasar14@gmail.com>
2017-10-16 18:56:47 +0200

    ipcs13 vjezba01

---
+++ ipcs_13/vjezba01/IPCS_13-V1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IPCS_13_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int x = 0;
+
+            while (x < arr.Length)
+            {
+                Console.WriteLine(arr[x]);
+                x++;
+            }
+        }
+    }
+}
