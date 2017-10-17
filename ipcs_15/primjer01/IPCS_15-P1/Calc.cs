7dfaa25d933d86bd00066e7e796245cbe50fb727
Edin Jašarević <edinjasar14@gmail.com>
2017-10-17 22:46:35 +0200

    ipcs15 primjer01

---
+++ ipcs_15/primjer01/IPCS_15-P1/Calc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPCS_15_P1
+{
+    class Calc
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
