8b644775a7e85147d03efc03b580cc6f55af5f1e
Edin Jašarević <edinjasar14@gmail.com>
2017-10-18 21:55:35 +0200

    ipcs20 primjer02

---
+++ ipcs_20/primjer02/IPCS_20-P2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace IPCS_20_P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream sourceFile = new FileStream("myFile.bin", FileMode.Open, FileAccess.Read);
+
+            //Kreiranje objekata klase BinaryReader i proslijedjivanje FileStream objekta kao parametar;
+            BinaryReader reader = new BinaryReader(sourceFile);
+
+            int position = 0; //incijalizacija pocetne pozicije ciljanog niza
+            int lenght = (int)reader.BaseStream.Length; //izracunavanje duzine toka;
+            byte[] dataCollection = new byte[lenght]; //kreiranje niza u koji ce biti smjesten svaki bajt iz fajla;
+            int returnByte; //petlja koja izvrsava citanje sve dok postoje bajtovi (dok marker ne vrati rezultat -1)
+
+            while ((returnByte = reader.Read()) != -1)
+            {
+                dataCollection[position] = (byte)returnByte; //smjestanje aktuelnog bajta na odgovarajucu poziciju niza
+                position += sizeof(byte); //pomjeranje pozicije ciljanog niza za velicinu procitanog podatka
+            }
+            //zatvaranje otvoreni tokova
+            reader.Close();
+            sourceFile.Close();
+
+            //prikaz procitanih podataka
+            foreach (byte data in dataCollection)
+                Console.WriteLine(data.ToString());
+
+        }
+    }
+}
