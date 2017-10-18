bb834ca46146b55968cd338b1ace79820f7adbdd
Edin Jašarević <edinjasar14@gmail.com>
2017-10-18 21:54:40 +0200

    ipcs18 vjezba01

---
+++ ipcs_18/vjezba01/IPCS_18-V1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_18_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+
+            try
+            {
+                checked
+                {
+                    int x = 2147483647;
+                    y = x * 2;
+                }
+            }
+            catch
+            {
+                y = 0;
+            }
+            Console.WriteLine(y);
+        }
+    }
+}
