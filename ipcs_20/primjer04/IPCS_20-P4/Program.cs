f0d6e18df5455d77ad7e58239f4d559265849497
Edin Jašarević <edinjasar14@gmail.com>
2017-10-18 21:57:00 +0200

    ipcs20 primjer04

---
+++ ipcs_20/primjer04/IPCS_20-P4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace IPCS_20_P4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = new string[] { "Paris", "London", "New York", "Los Angeles" };
+
+            StreamWriter sw = new StreamWriter("myFile.txt");
+            foreach (string city in cities)
+                sw.WriteLine(city);
+
+            sw.Close();
+        }
+    }
+}
