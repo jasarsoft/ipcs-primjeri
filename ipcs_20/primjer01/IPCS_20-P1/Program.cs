908e84278406b627719f8b5c6d99e5b0e160eccf
Edin Jašarević <edinjasar14@gmail.com>
2017-10-18 21:55:19 +0200

    ipcs20 primjer01

---
+++ ipcs_20/primjer01/IPCS_20-P1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace IPCS_20_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] dataCollection = { 1, 4, 6, 7, 12, 33, 26, 98, 82, 101 };
+
+            FileStream destFile = new FileStream("myFile.bin", FileMode.Create, FileAccess.Write);
+
+            BinaryWriter writer = new BinaryWriter(destFile);
+            foreach (byte data in dataCollection)
+            {
+                writer.Write(data);
+            }
+            writer.Close();
+            destFile.Close();
+        }
+    }
+}
