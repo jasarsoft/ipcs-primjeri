771a6b24fe4d915c2ea2e5577a35af65df147a97
Edin Jašarević <edinjasar14@gmail.com>
2017-10-18 21:57:33 +0200

    ipcs20 primjer06

---
+++ ipcs_20/primjer06/IPCS_20-P6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace IPCS_20_P6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destinationFilePath = "myFile.bin";
+
+            FileStream destFile = new FileStream(destinationFilePath, FileMode.Create, FileAccess.Write);
+            BinaryWriter writer = new BinaryWriter(destFile);
+
+            int intValue = 999999999;
+
+            writer.Write(intValue);
+            writer.Close();
+            destFile.Close();
+        }
+    }
+}
