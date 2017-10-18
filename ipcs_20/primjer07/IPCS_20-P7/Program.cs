5d8744ae842af4f66d47f71637b824c8ee17114a
Edin Jašarević <edinjasar14@gmail.com>
2017-10-18 21:57:50 +0200

    ipcs20 primjer07

---
+++ ipcs_20/primjer07/IPCS_20-P7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace IPCS_20_P7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = "myFile.bin";
+
+            FileStream sourceFile = new FileStream(sourcePath, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(sourceFile);
+            int value = reader.ReadInt32();
+            reader.Close();
+            sourcePath.Clone();
+            Console.WriteLine(value);
+        }
+    }
+}
