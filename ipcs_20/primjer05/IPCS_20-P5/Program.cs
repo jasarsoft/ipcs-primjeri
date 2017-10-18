21d42a52afe6d11878c5a13b9d36ecbde0175e24
Edin Jašarević <edinjasar14@gmail.com>
2017-10-18 21:57:15 +0200

    ipcs20 primjer05

---
+++ ipcs_20/primjer05/IPCS_20-P5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+
+namespace IPCS_20_P5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            ArrayList cities = new ArrayList();
+            StreamReader sr = new StreamReader("myFile.txt");
+
+            while ((line = sr.ReadLine()) != null)
+                cities.Add(line);
+
+            foreach (string city in cities)
+                Console.WriteLine(city);
+        }
+    }
+}
