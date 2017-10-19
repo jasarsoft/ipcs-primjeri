c4f49dc0026d34d3438ebe20007def22999e18ae
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 15:13:55 +0200

    ipcs26 primjer01

---
+++ ipcs_26/primjer01/IPCS_26-P1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace IPCS_26_P1
+{
+    class Product
+    {
+        private decimal price;
+        private decimal tax = 0.18M;
+
+        public void setPrice(decimal price)
+        {
+            this.price = price;
+        }
+
+        public decimal getPrice()
+        {
+            return this.price + (this.price * tax);
+        }
+    }
+}
---
+++ ipcs_26/primjer01/IPCS_26-P1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace IPCS_26_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+
+            p.setPrice(10);
+            Console.WriteLine(p.getPrice());
+        }
+    }
+}
