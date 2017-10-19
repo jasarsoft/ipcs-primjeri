58ea23cc224b4020be1b1c3b2fce255d28e411e0
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 15:15:38 +0200

    ipcs26 primjer05

---
+++ ipcs_26/primjer05/IPCS_26-P5/MyExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace IPCS_26_P5
+{
+    public static class MyExtension
+    {
+        //kreiranje ekstenzionog metoda
+        public static string up(this String str)
+        {
+            if (str.Length > 5)
+                return str.ToUpper();
+            else
+                return str;
+        }
+    }
+}
---
+++ ipcs_26/primjer05/IPCS_26-P5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace IPCS_26_P5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "good morning";
+
+            //koristenje ekstenzionog metoda
+            Console.WriteLine(s.up());
+        }
+    }
+}
