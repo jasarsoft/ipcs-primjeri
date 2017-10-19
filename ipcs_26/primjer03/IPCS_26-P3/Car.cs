b421e23316c07681a16897709ebfcbefcaaf2724
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 15:14:32 +0200

    ipcs26 primjer03

---
+++ ipcs_26/primjer03/IPCS_26-P3/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPCS_26_P3
+{
+    public class Car
+    {
+        //polje klase color ce biti staticko
+        public static string color;
+    }
+}
---
+++ ipcs_26/primjer03/IPCS_26-P3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPCS_26_P3
+{
+    class Program
+    {
+        //method paintCar mjenja polje kalse color
+        static void paintCar()
+        {
+            Car.color = "blue";
+        }
+
+        static void Main(string[] args)
+        {
+            //poziva se metod paintCar
+            paintCar();
+            //prikazije se trenutna vrijednost polja color klase Car
+            Console.WriteLine(Car.color);
+        }
+    }
+}
