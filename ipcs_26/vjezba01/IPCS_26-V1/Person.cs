2ba0a8d21d6f53c9a48ceee30c65893105d01454
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 15:15:20 +0200

    ipcs26 vjezba01

---
+++ ipcs_26/vjezba01/IPCS_26-V1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace IPCS_26_V1
+{
+    class Person
+    {
+        public static int retirementAge;
+        
+        public Person(int retAge)
+        {
+            retirementAge = retAge;
+        }
+
+        public static int CalculateRemaingWorkYears(int age)
+        {
+            return (retirementAge - age);
+        }
+    }
+}
---
+++ ipcs_26/vjezba01/IPCS_26-V1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace IPCS_26_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
