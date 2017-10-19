2f27cdd758dfd3f3fa20c9cf338601692ff7cd44
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 15:14:12 +0200

    ipcs26 primjer02

---
+++ ipcs_26/primjer02/IPCS_26-P2/Sales.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPCS_26_P2
+{
+    public class Sales
+    {
+        private Revenue salesRevenue;
+
+        public void SetRevenue(string currency, double amount)
+        {
+            this.salesRevenue = new Revenue(currency, amount);
+        }
+
+
+        private struct Revenue
+        {
+            double amount;
+            string currency;
+            
+            public Revenue(string currency, double amount)
+            {
+                this.currency = currency;
+                this.amount = amount;
+            }
+        }
+    }
+}
