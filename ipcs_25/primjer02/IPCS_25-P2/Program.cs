4f0e757bb171b4f6dbebd104593e996d4a676ee8
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 15:13:36 +0200

    ipcs25 primjer02

---
+++ ipcs_25/primjer02/IPCS_25-P2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPCS_25_P2
+{
+    struct Test
+    {
+        public int x;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.x = 25;
+            Console.WriteLine(t.x);
+
+            object o = t;
+            Test t1 = (Test)o;
+            Console.WriteLine(t1.x);
+        }
+    }
+}
