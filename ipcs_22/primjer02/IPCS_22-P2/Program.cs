fd3c30f9b3b97b8bcce566609dd07d7209b183b3
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 15:12:23 +0200

    ipcs22 primjer02

---
+++ ipcs_22/primjer02/IPCS_22-P2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace IPCS_22_P2
+{
+    class Program
+    {
+        enum Planets
+        {
+            Mercury,
+            Venus,
+            Earth,
+            Mars,
+            Jupiter,
+            Saturn,
+            Uranus,
+            Neptune,
+            Unknown
+        };
+
+        static void Main(string[] args)
+        {
+            Planets p;
+            string planet = "Mars";
+
+            if (Enum.IsDefined(typeof(Planets), planet))
+                p = (Planets)Enum.Parse(typeof(Planets), planet, true);
+            else
+                p = Planets.Unknown;
+
+            Console.WriteLine(p);
+        }
+    }
+}
