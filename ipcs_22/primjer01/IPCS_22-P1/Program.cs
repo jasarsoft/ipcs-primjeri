f51af3f7678ad5303989d10264767220f26bf570
Edin Jašarević <edinjasar14@gmail.com>
2017-10-19 15:12:09 +0200

    ipcs22 primjer01

---
+++ ipcs_22/primjer01/IPCS_22-P1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace IPCS_22_P1
+{
+    class Program
+    {
+        enum Planets
+        {
+            Mercury,
+            Venus,
+            Earth,
+            Mars,
+            Jupiter,
+            Saturn,
+            Uranus,
+            Neptune
+        };
+        
+        static void Main(string[] args)
+        {
+            for (Planets planet = Planets.Mercury; planet <= Planets.Neptune; planet++)
+            {
+                Console.WriteLine(planet);
+            }
+
+            Console.WriteLine(Enum.IsDefined(typeof(Planets), "Mercury"));
+            Console.WriteLine(Enum.Parse(typeof(Planets), "Mercury"));
+        }
+    }
+}
