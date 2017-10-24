3630508b1582226e537c907d1b8ac1bbfc782e10
Edin Jašarević <edinjasar14@gmail.com>
2017-10-24 22:08:13 +0200

    ipcs31 vjezba01

---
+++ ipcs_31/vjezba01/IPCS_31-V1/Predavac.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_31_V1
+{
+    public class Predavac
+    {
+        string[] pred = new string[3];
+
+        public string this[int i]
+        {
+            get { return pred[i]; }
+        }
+
+
+        public Predavac()
+        {
+            pred[0] = "Marko Markovic";
+            pred[1] = "Pera Perovic";
+            pred[2] = "Boza Bozovic";
+        }
+    }
+}
---
+++ ipcs_31/vjezba01/IPCS_31-V1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IPCS_31_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var predaje = new Predavac();
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Predavac: {0}", predaje[i]);
+            }
+        }
+    }
+}
