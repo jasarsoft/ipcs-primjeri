b864bc098403ca27e08fd2b90f2c03fa1df6a6cd
Edin Jašarević <edinjasar14@gmail.com>
2017-10-24 22:06:43 +0200

    ipcs29 primjer01

---
+++ ipcs_29/primjer01/IPCS_29-P1/MyGCCollectClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPCS_29_P1
+{
+    class MyGCCollectClass
+    {
+        private const long maxGarbage = 1000;
+
+        public void MakeSomeGarbage()
+        {
+            Version vt;
+
+            for (int i = 0; i < maxGarbage; i++)
+            {
+                //kreirati objekte i osloboditi ih da popune memoriju sa nekoristenim objektima
+                vt = new Version();
+            }
+        }
+    }
+}
---
+++ ipcs_29/primjer01/IPCS_29-P1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPCS_29_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGCCollectClass myGCCol = new MyGCCollectClass();
+
+            //odrediti maksimalna broj generisanja koje GC sistem podrzava
+            Console.WriteLine("The higest generation is {0}", GC.MaxGeneration);
+
+            myGCCol.MakeSomeGarbage();
+
+            //odrediti kojom metodom generisanja je objekat myGCCol sacuvan
+            Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+
+            //priblizno odrediti broj bajtova trenutno lociranih u koristenoj memoriji
+            Console.WriteLine("Totoal Memory: {0}", GC.GetTotalMemory(false));
+
+            //izvrsiti prikupljanje kada je generiasnje nula
+            GC.Collect(0);
+
+            //odrediti kojom metodom generisanja je objekat myGCCol sacuvan
+            Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+
+            //izvrsiti prikupljanje svih generisanja do 2 i ukljuciti 2
+            GC.Collect(2);
+
+            //odrediti kojom metodom generisanja je objekat myGCCol sacuvan
+            Console.WriteLine("Generation: {0}", GC.GetGeneration(myGCCol));
+            Console.WriteLine("Total Memory: {0}", GC.GetTotalMemory(false));
+            Console.Read();
+        }
+    }
+}
