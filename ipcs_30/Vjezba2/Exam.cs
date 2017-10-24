ce19637314d83d0fbae1f8ea7b3d7e7092c57f59
Edin Jašarević <edinjasar14@gmail.com>
2017-10-24 22:07:42 +0200

    ipcs30 vjezba02

---
+++ ipcs_30/Vjezba2/Exam.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba2
+{
+    public class Exam
+    {
+        private int month;
+
+        public int Month
+        {
+            get { return month; }
+            set
+            {
+                if (value > 1 && value <= 12)
+                    month = value;
+                else
+                    Console.WriteLine("Niste unijeli validan mjesec odrzavanja ispita");
+            }
+        }
+
+
+        //pravimo metodu koju mogu da koriste objekti ove klase 
+        //i koja sluzi za implementaciju get metode
+        public static Exam Read()
+        {
+            Exam e = new Exam();
+            Console.WriteLine("Mjesec odrzavanja prijemnog ispita: ");
+            e.Month = int.Parse(Console.ReadLine());
+            return e;
+        }
+
+        //pravimo metodu koju mogu da koriste objekti ove klase
+        //i koja sluzi za implementaciju set metode
+        public static void Write(Exam ex)
+        {
+            Exam e = new Exam();
+            Console.WriteLine("Mjesec koji ste unijeli ispravan je i iznosi: {0}", ex.Month);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ipcs_30/Vjezba2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pozivamo metodu Read okja sadrzi implementaciju get metode
+            Exam e = Exam.Read();
+            
+            //postosmo set metodu implementirali u ovoj metodi klase Exam, sada samo pozivamo metodu
+            Exam.Write(e);
+
+        }
+    }
+}
